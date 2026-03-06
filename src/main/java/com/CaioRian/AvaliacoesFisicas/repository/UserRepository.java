7ccc826f4b616486a1ba2b33c10e4b6dd8840a9d
CaioRian187 <caiorian246810@gmail.com>
2026-03-06 20:12:35 -0300

    Refatorando UserRepository

--- src/main/java/com/CaioRian/AvaliacoesFisicas/repository/UserRepository.java
+++ src/main/java/com/CaioRian/AvaliacoesFisicas/repository/UserRepository.java
@@ -4,8 +4,12 @@ import com.CaioRian.AvaliacoesFisicas.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID>{
+
+    Optional<User> findByNome(String nome);
+
     UserDetails findByLogin(String login);
 }
